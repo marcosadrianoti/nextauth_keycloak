c82ea5184b812b097e1894ae8a18ec73451e1f16
Marcos Adriano <marcosadriano.ti@gmail.com>
2024-03-11 16:32:29 -0300

    add sub property to user

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,33 +1,21 @@
 import NextAuth from "next-auth/next";
-import { NextAuthOptions, Profile } from "next-auth";
+import { NextAuthOptions, Profile, User } from "next-auth";
 import KeycloakProvider, { type KeycloakProfile } from "next-auth/providers/keycloak"
 import { type JWT } from "next-auth/jwt";
 import { OAuthConfig } from "next-auth/providers/oauth";
-
-declare module 'next-auth/jwt' {
-  interface JWT {
-    id_token?: string;
-    provider?: string;
-  }
-}
-
-interface CommonJWT extends JWT {
-  provider: string;
-  id_token: string;
-}
-
-interface CustomUser {
-  id: string;
-  name: string;
-  email: string;
-  password: string;
-  role: string;
-}
 
 interface CustomProfile extends Profile {
   realm_access?: {
     roles: string[];
   };
+}
+
+interface CustomUser extends User {
+  name: string;
+  email: string;
+  role: string;
+  sub: string;
+  teste: string;
 }
 
 const keycloak = KeycloakProvider({
@@ -37,9 +25,7 @@ const keycloak = KeycloakProvider({
 });
 
 export const authOptions: NextAuthOptions = {
-  providers: [
-    keycloak
-  ],
+  providers: [ keycloak ],
   callbacks: {
     jwt: async ({ token, user, account, profile }) => {
       if (user && account) {
@@ -52,26 +38,28 @@ export const authOptions: NextAuthOptions = {
           access_token: account.access_token,
           expires_at: account.expires_at,
           refresh_token: account.refresh_token,
-          role: role
+          role: role,
         }
         return token;
       }
       return token;
     },
     session: async ({ session, token }) => {
-      return {
+      session = {
         ...session,
-        user: {
+        user:<CustomUser>{
           ...session.user,
           role: token.role,
+          sub: token.sub,
           teste: "teste"
         }
       }
+      return session;
     }
   },
   events: {
     async signOut({ token }: { token: JWT }) {
-      const issuerUrl = (authOptions.providers.find(p => p.id === "keycloak") as OAuthConfig<KeycloakProfile>).options!.issuer!
+      const issuerUrl = (authOptions.providers.find(provider => provider.id === "keycloak") as OAuthConfig<KeycloakProfile>).options!.issuer!
       const logOutUrl = new URL(`${issuerUrl}/protocol/openid-connect/logout`)
       logOutUrl.searchParams.set("id_token_hint", token.id_token!)
       // http://localhost:8080/realms/my-realm/protocol/openid-connect/logout?id_token_hint=your-id_token_hint
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,5 +3,6 @@ declare module "next-auth/jwt" {
   interface JWT {
       provider?: string;
       id_token?: string;
+      // sub?: string;
   }
 }
